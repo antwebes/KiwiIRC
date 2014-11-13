272891d63071e8c25d07bf653f4efde6aa4d9434
Borja <bmp_86@hotmail.com>
2014-11-13 17:44:41 +0100

    Add avatar

--- client/src/applets/avatar.js
+++ client/src/applets/avatar.js
@@ -1,184 +1,19 @@
 (function () {
 
-    var avatarContent = {
-      "total": 6,
-      "limit": 6,
-      "offset": 0,
-      "_links": {
-        "self": {
-          "href": "http://api.chatsfree.lo/api/users/31/photos"
-        }
-      },
-      "resources": [
-        {
-          "id": 674,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-07-02T16:42:41+0200",
-          "number_votes": 0,
-          "album": {
-            "id": 31,
-            "participant": {
-              "id": 31,
-              "username": "alex",
-              "username_canonical": "alex",
-              "language": "en"
-            },
-            "title": "alex Album",
-            "description": "Default album of alex"
-          },
-          "path": "http://api.chatsfree.lo/uploads//original/2014/07/02/53b41a610b852.jpg",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/07/02/53b41a610b852_large.jpg",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/07/02/53b41a610b852_medium.jpg",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/07/02/53b41a610b852_small.jpg",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/07/02/53b41a610b852_icon.jpg",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/674"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            },
-            "album": {
-              "href": "http://api.chatsfree.lo/api/users/31/albums/31"
-            }
-          }
-        },
-        {
-          "id": 675,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-07-04T12:17:45+0200",
-          "number_votes": 0,
-          "path": "http://api.chatsfree.lo/uploads//original/2014/07/04/53b67f4927de1.png",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/07/04/53b67f4927de1_large.png",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/07/04/53b67f4927de1_medium.png",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/07/04/53b67f4927de1_small.png",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/07/04/53b67f4927de1_icon.png",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/675"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            }
-          }
-        },
-        {
-          "id": 676,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-07-04T12:23:12+0200",
-          "number_votes": 0,
-          "path": "http://api.chatsfree.lo/uploads//original/2014/07/04/53b680902b88d.jpg",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/07/04/53b680902b88d_large.jpg",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/07/04/53b680902b88d_medium.jpg",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/07/04/53b680902b88d_small.jpg",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/07/04/53b680902b88d_icon.jpg",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/676"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            }
-          }
-        },
-        {
-          "id": 677,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-07-21T12:06:16+0200",
-          "number_votes": 0,
-          "path": "http://api.chatsfree.lo/uploads//original/2014/07/21/53cce618bb732.JPG",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/07/21/53cce618bb732_large.JPG",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/07/21/53cce618bb732_medium.JPG",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/07/21/53cce618bb732_small.JPG",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/07/21/53cce618bb732_icon.JPG",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/677"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            }
-          }
-        },
-        {
-          "id": 678,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-07-21T12:12:55+0200",
-          "number_votes": 0,
-          "path": "http://api.chatsfree.lo/uploads//original/2014/07/21/53cce7a80c7fa.JPG",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/07/21/53cce7a80c7fa_large.JPG",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/07/21/53cce7a80c7fa_medium.JPG",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/07/21/53cce7a80c7fa_small.JPG",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/07/21/53cce7a80c7fa_icon.JPG",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/678"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            }
-          }
-        },
-        {
-          "id": 683,
-          "participant": {
-            "id": 31,
-            "username": "alex",
-            "username_canonical": "alex",
-            "language": "en"
-          },
-          "publicated_at": "2014-08-12T17:28:36+0200",
-          "number_votes": 0,
-          "path": "http://api.chatsfree.lo/uploads//original/2014/08/12/53ea32a4c022b.jpg",
-          "path_large": "http://api.chatsfree.lo/uploads//2014/08/12/53ea32a4c022b_large.jpg",
-          "path_medium": "http://api.chatsfree.lo/uploads//2014/08/12/53ea32a4c022b_medium.jpg",
-          "path_small": "http://api.chatsfree.lo/uploads//2014/08/12/53ea32a4c022b_small.jpg",
-          "path_icon": "http://api.chatsfree.lo/uploads//2014/08/12/53ea32a4c022b_icon.jpg",
-          "_links": {
-            "self": {
-              "href": "http://api.chatsfree.lo/api/photos/683"
-            },
-            "creator": {
-              "href": "http://api.chatsfree.lo/api/users/31"
-            }
-          }
-        }
-      ]
-    };
+
+    window.guest_api = "http://www.chatsfree.lo/app_dev.php";
 
     var Mediator = {};
     _.extend(Mediator, Backbone.Events);
 
 
     var Avatar = Backbone.Model.extend({
-        urlRoot : '/api/avatar',
+        urlRoot : window.guest_api + '/api/changeAvatar',
 
         selectAvatar: function () {
+           this.set({
+             nick: _kiwi.global.components.Network().get("nick")
+           });           
            this.save(); 
            Mediator.trigger('avatar:select', this.attributes);
         }
@@ -230,20 +65,41 @@
     var View = Backbone.View.extend({
         avatarsCollection: {},
 
-        loadAvatar: function (filter) {
-            if(filter === undefined) {
-                filter = "animals";
-            }
+        events: {
+          "click .avatar_category": 'changeAvatarCategory'
+        },
 
-            var avatars = avatarContent.resources.map(function(avatar) {
-                delete avatar.participant;
-                return avatar;
+        changeAvatarCategory: function (ev) {
+          this.loadAvatar($(ev.target).data("filters"));
+        },
+
+        loadAvatar: function (filters) {
+            filters = filters || "";
+            if(filters == "") filters += "discriminator=avatar";
+            else filters += ",discriminator=avatar";
+
+            $("#avatar_container", this.$el).empty();
+
+            $.ajax({
+              url: window.guest_api + '/photos?filters=' + filters,
+              dataType: 'json',
+              contentType: "application/json"
+            })
+            .done(function( data ){
+
+              var avatars = data.resources.map(function(avatar) {
+                  delete avatar.participant;
+                  return avatar;
+              });
+
+              avatarsCollection = new AvatarsCollection(avatars);
+
+
+              var avatarsView = new AvatarsView({ collection: avatarsCollection });
+
+              $("#avatar_container", this.$el).append(avatarsView.render().el);
+              $("#img_avatar", this.$el).attr("src", $("#current_avatar").attr("src"));
             });
-
-            avatarsCollection = new AvatarsCollection(avatars);
-            var avatarsView = new AvatarsView({ collection: avatarsCollection });
-
-            $("#avatar_container", this.$el).append(avatarsView.render().el);
         },
 
         selectAvatar: function(avatar){
