d11c42292eba582ab40d00eb5c838a845af44fe8
jdeveloper <jdeveloper.inxenio@gmail.com>
2015-02-02 17:52:29 +0100

    Added default inglish items to translation files if a key is not translated

--- server/helpers/build.js
+++ server/helpers/build.js
@@ -232,37 +232,68 @@ var pluginsTransPromise = new Promise(function(resolve, reject){
 
 fs.readdir(global.config.public_http + '/src/translations', function (err, translation_files) {
     if (!err) {
-        translation_files.forEach(function (file) {
-            var locale = file.slice(0, -3);
+        var tanslatedLanguages = {};
+        var proccessedFiles = 0;
 
-            if ((file.slice(-3) === '.po') && (locale !== 'template')) {
-                po2json.parseFile(global.config.public_http + '/src/translations/' + file, {format: 'jed', domain: locale}, function (err, json) {
-                    if (!err) {
-                        (function(json, locale) {
-                            var toJSONFfile = function (pluginsTrans) {
-                                if(typeof pluginsTrans[locale] != 'undefined') {
-                                    for (msgid in pluginsTrans[locale]) {
-                                        json.locale_data[locale][msgid] = pluginsTrans[locale][msgid];
-                                    }
-                                }
+        var finalTransPromise = new Promise(function(resolve, reject) {
+            translation_files.forEach(function (file) {
+                var locale = file.slice(0, -3);
 
-                                fs.writeFile(global.config.public_http + '/assets/locales/' + locale + '.json', JSON.stringify(json), function (err) {
-                                    if (!err) {
-                                        console.log('Built translation file %s.json', locale);
-                                    } else {
-                                        console.error('Error building translation file %s.json:', locale, err);
+                if ((file.slice(-3) === '.po') && (locale !== 'template')) {
+                    po2json.parseFile(global.config.public_http + '/src/translations/' + file, {
+                        format: 'jed',
+                        domain: locale
+                    }, function (err, json) {
+                        if (!err) {
+                            (function (json, locale) {
+                                var toJSONFfile = function (pluginsTrans) {
+                                    if (typeof pluginsTrans[locale] != 'undefined') {
+                                        for (msgid in pluginsTrans[locale]) {
+                                            json.locale_data[locale][msgid] = pluginsTrans[locale][msgid];
+                                        }
                                     }
+
+                                    tanslatedLanguages[locale] = json;
+                                    proccessedFiles++;
+
+                                    if(proccessedFiles == translation_files.length){
+                                        resolve(tanslatedLanguages);
+                                    }
+                                };
+
+                                pluginsTransPromise.then(toJSONFfile, function () {
+                                    toJSONFfile({});
                                 });
-                            };
+                            })(json, locale);
+                        } else {
+                            console.error('Error building translation file %s.json: ', locale, err);
+                        }
+                    });
+                }else{
+                    proccessedFiles++;
+                }
+            });
+        });
 
-                            pluginsTransPromise.then(toJSONFfile, function () {
-                                toJSONFfile({});
-                            });
-                        })(json, locale);
-                    } else {
-                        console.error('Error building translation file %s.json: ', locale, err);
+        finalTransPromise.then(function(translations){
+            var defaultLanguage = translations['en-gb'].locale_data['en-gb'];
+
+            for(translation in translations){
+                (function(translations, translation){
+                    for(translationKey in defaultLanguage){
+                        if(typeof translations[translation].locale_data[translation][translationKey] == 'undefined'){
+                            translations[translation].locale_data[translation][translationKey] = defaultLanguage[translationKey];
+                        }
                     }
-                });
+
+                    fs.writeFile(global.config.public_http + '/assets/locales/'+translation+'.json', JSON.stringify(translations[translation]), function (err) {
+                        if (!err) {
+                            console.log('Built translation file %s.json', translation);
+                        } else {
+                            console.error('Error building translation file %s.json:', translation, err);
+                        }
+                    });
+                })(translations, translation);
             }
         });
     } else {
