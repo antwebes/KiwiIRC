aa4bccf55d8f7c53cc87cc5ee5a9e070f7582853
Borja <bmp_86@hotmail.com>
2016-01-15 13:48:01 +0100

    Export container ports and build

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 echo "git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js"
 git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js
 
+./kiwi build
 npm start
