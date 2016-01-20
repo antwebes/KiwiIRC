595f6a3d79f4a9a33c4b7115a8dae181440db0a9
Borja <bmp_86@hotmail.com>
2016-01-20 12:19:30 +0100

    Change entrypoint permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod 600 /root/.ssh/*
 echo "git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js"
 git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js
 
