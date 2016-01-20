272da186c2a4f89b272d0de67d2a615c14e1b54f
Borja <bmp_86@hotmail.com>
2016-01-20 12:34:07 +0100

    Change entrypoint permission key files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chmod 600 /root/.ssh/*
+chown root:root /root/.ssh/*
+
 echo "git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js"
 git archive --format=zip --remote=git@bitbucket.org:$REPOSITORY $WEB_CONFIG_BRANCH:$WEB_CONFIG_APP config.js | gunzip > config.js
 
